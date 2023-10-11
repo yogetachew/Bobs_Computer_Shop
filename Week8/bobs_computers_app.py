1b943f6ac5a8d31950a9c8e8f18730cfb7b32717
siah.drews <siah.drews@gmail.com>
2023-10-11 15:37:15 -0600

    working application file

--- Week8/bobs_computers_app.py
+++ Week8/bobs_computers_app.py
@@ -3,3 +3,29 @@
   Created: 10/11/23
   Purpose: Main application file for menu and accessing objects
 """
+
+# TODO: import class file
+import Bobs_computers_class
+
+# TODO: create main
+def main():
+    # Create computer object
+    computer = Bobs_computers_class.Computer()
+    # Call menu function, pass reference to computer object
+    menu(computer)
+
+# TODO: create menu function
+def menu(computer):
+    while True:
+        # Get number of computers from user
+        # set quantity to object
+        computer.quantity = (int(input(" How many computers: ")))
+
+        # Get computer quantity from object
+        quantity = computer.quantity
+        print(f" You ordered {quantity} computers")
+
+
+
+if __name__ == "__main__":
+    main()
