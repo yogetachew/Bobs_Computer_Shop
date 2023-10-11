74d1902fbae4a56d8255ec43e72ca19f4bf6e211
regtoga <91752566+regtoga@users.noreply.github.com>
2023-10-11 14:59:33 -0600

    Bobs_Computer_classV1 is fin

--- Week8/Bobs_computers_class.py
+++ Week8/Bobs_computers_class.py
@@ -7,3 +7,18 @@
 
 class Computer:
     """Define getter with @property decorator"""
+    @property
+    def quantity(self):
+        return self._quantity
+    
+    """Define setter with @property.setter decorator"""
+    @quantity.setter
+    def quantity(self, quantity: int):
+        self._quantity = quantity
+
+#ComputerCase (Probbly jsut let the choose the color to make it simpler)
+#MotherboardType (Maby just let the choose if they want wifi or not at a $30 dollar discount if they dont to make it simpler)
+#CPU(could do a specific SKU or just simplify and calculate cost baised on per core basis)
+#GPU(Could do a specific SKU or just simplify and calculate cost baised on ram they want/need)
+#MemoryAmmount
+#StorageType(HHD, SSD, NVME) (just do this calculation baised on cost per gigabyte)
