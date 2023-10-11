ca661c77fb9c017867c08597ec3cc13121d55de6
regtoga <91752566+regtoga@users.noreply.github.com>
2023-10-11 14:48:18 -0600

    just an intitial comit to sync folders and files

---
+++ Week8/Bobs_computers_class.py
@@ -0,0 +1,9 @@
+"""
+    Name: Bobs_computers_class.py
+    Author: Aidan Newberry
+    Created: 10/11/2023
+    Purpose: Use @property Pythoic way of accessing attrabutes
+"""
+
+class Computer:
+    """Define getter with @property decorator"""
