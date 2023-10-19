2166065f16a1838bcc3ffba72e012d958bb9dec1
regtoga <91752566+regtoga@users.noreply.github.com>
2023-10-19 13:59:47 -0600

    Streamlined making menuitems

--- Week9/Bobs_computers_class.py
+++ Week9/Bobs_computers_class.py
@@ -8,7 +8,7 @@
 class Computer:
     # create init
     def __init__(self):
-        self.colors = {
+        self.CaseColors = {
             "Black":100,
             "White":115,
             "Red":120
@@ -32,35 +32,47 @@ class Computer:
 #ComputerCase (Probbly jsut let the choose the color to make it simpler)
     def ComputerCaseMenu(self) -> int:
         print("When making building a computer you can choose the color of the case. Here are your options to choose from: ")
+        
+        inputprompt = "Enter name of the color that tickles your fancy: "
+        errormessage = "That input wasnt a case color we have in stock! \ntry again:"
 
-        while True:
-            print("(Black) $100 \n(White) $115 \n(Red) $120")
-            userinput = input("Enter name of the color that tickles your fancy: ")
-            
-            errormessage = f"{userinput}, wasnt a case color we have in stalk! \ntry again:"
-            
-            casecolorprice = self.colors.get(userinput, errormessage)
-
-            if casecolorprice != errormessage:
-                return casecolorprice 
-
+        return Menu(self.CaseColors, inputprompt, errormessage)
 
 #MotherboardType (Maby just let the choose if they want wifi or not at a $30 dollar discount if they dont to make it simpler)
     def MotherboardTypeMenu(self) -> int:
         print("Additionally You can pick whether or not you need your motherboard to support wifi. Here are your options to choose from: ")
 
-        while True:
-            print("(Regular) $120 \n(Wifi) $145")
-            userinput = input("Enter name of the motherboard that tickles your fancy: ")
-
-            errormessage = f"{userinput}, wasnt a case color we have in stalk! \ntry again:"
-            
-            Motherboardprice = self.Motherboards.get(userinput, errormessage)
-
-            if Motherboardprice != errormessage:
-                return Motherboardprice 
+        inputprompt = "Enter name of the motherboard that tickles your fancy: "
+        errormessage = "That input wasnt a Motherboard we have in stock! \ntry again:"
+        
+        return Menu(self.Motherboards, inputprompt, errormessage)
 
 
+
+def Menu(dictionary:dict, inputprompt:str, errormessage:str):
+    """handles majority of menu making"""
+    while True:
+        print(FormatMenu(dictionary))
+
+        userinput = input(inputprompt)
+
+        item = dictionary.get(userinput, errormessage)
+
+        if item != errormessage:
+                return item
+        
+        print(errormessage)
+
+
+
+def FormatMenu(input:dict) -> str:
+    """makes a formatted menu when passed a dictionary"""
+    menustr = ""
+    for item in input:
+        price = input.get(f'{item}')
+        menustr += f"({item}) ${price} \n"
+    return menustr
+        
 
 #CPU(could do a specific SKU or just simplify and calculate cost baised on per core basis)
 #GPU(Could do a specific SKU or just simplify and calculate cost baised on ram they want/need)
