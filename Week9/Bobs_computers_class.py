d0dd7ab19598755baaf10639172a19055daef992
siah.drews <siah.drews@gmail.com>
2023-10-21 22:54:40 -0600

    user input index # rather than key

--- Week9/Bobs_computers_class.py
+++ Week9/Bobs_computers_class.py
@@ -86,10 +86,10 @@ def Menu(dictionary:dict, inputprompt:str, errormessage:str):
     while True:
         print(FormatMenu(dictionary))
 
-        userinput = input(inputprompt)
+        userinput = int(input(inputprompt))-1
 
-        item = dictionary.get(userinput, errormessage)
-
+        """item = dictionary.get(userinput, errormessage)"""
+        item = list(dictionary.values())[userinput]
         if item != errormessage:
                 return item
         
@@ -98,11 +98,16 @@ def Menu(dictionary:dict, inputprompt:str, errormessage:str):
 
 
 def FormatMenu(input:dict) -> str:
+    """
+        takes in dictionary
+        splits into index #, key and value
+        returns formatted menu
+    """
     """makes a formatted menu when passed a dictionary"""
     menustr = ""
-    for item in input:
-        price = input.get(f'{item}')
-        menustr += f"({item}) ${price} \n"
+
+    for index, (key, value) in enumerate(input.items(), start=1):
+        menustr += f"[{index}]: {key}: ${value:.2f} \n"
     return menustr
         
 
