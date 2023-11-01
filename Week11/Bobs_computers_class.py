008bea5b04de8986c4a468b25b58ddcd1889edac
regtoga <91752566+regtoga@users.noreply.github.com>
2023-11-01 14:00:10 -0600

    Week11 Comit

---
+++ Week11/Bobs_computers_class.py
@@ -0,0 +1,211 @@
+"""
+    Name: Bobs_computers_class.py
+    Author: Aidan Newberry
+    Created: 10/11/2023
+    Purpose: Use @property Pythoic way of accessing attrabutes
+"""
+
+class Computer:
+    # create init
+    def __init__(self):
+        self.CaseColors = {
+            "Black":100,
+            "White":115,
+            "Red":120
+        }
+
+        self.CPUVendors = ["AMD", "Intel"]
+
+        self.Motherboards = {
+            "Regular":120,
+            "Wifi":145
+        }
+
+        self.cpuOptions = {
+            "4-Core":100,
+            "8-Core":200,
+            "16-Core":500
+        }
+
+        self.cpuCoolerOptions = {
+            "Stock Cooler":0,
+            "Air Cooler": 50,
+            "AiO":150,
+            "Custom Liquid Loop":500
+        }
+
+        self.gpuOptions = {
+            "GTX 1650":150,
+            "GTX 1080":250,
+            "RTX 2070 SUPER":500,
+            "RTX 3080":900,
+            "RTX 4090ti":2000
+        }
+
+        self.storageoptions = {
+            "HDD 1TB": 50,
+            "HDD 2TB": 80,
+            "SSD 256GB": 70,
+            "SSD 512GB": 120,
+            "SSD 1TB": 220,
+        }
+
+        self.memoryoptions = {
+            "8GB": 50,
+            "16GB": 100,
+            "32GB": 200,
+            "64GB": 350,
+        }
+    
+    """Define getter with @property decorator"""
+    @property
+    def quantity(self):
+        return self._quantity
+    
+    """Define setter with @property.setter decorator"""
+    @quantity.setter
+    def quantity(self, quantity: int):
+        self._quantity = quantity
+
+    @staticmethod
+    def title():
+        print("  _____                       _____ ")
+        print(" |_____|   Bob's Computer    |_____|")
+        print("/_____/        Shop         /_____/ ")
+
+#ComputerCase (Probbly jsut let the choose the color to make it simpler)
+    def ComputerCaseMenu(self) -> int:
+        print("\nWhen making building a computer you can choose the color of the case. Here are your options to choose from: ")
+        
+        inputprompt = "Enter name of the color that tickles your fancy: "
+        errormessage = "That input wasnt a case color we have in stock! \ntry again:"
+
+        return Menu(self.CaseColors, inputprompt, errormessage)
+
+#CPUVendor, give the user the illusion of choice when picking a cpu/motherboard, if they pick AMD they will get an AMD CPU
+    
+    def CPUVendor(self) -> str:
+        print("When choosing a motherboard it is crucial for us to know what Vendor of CPU you will be using. Here are you options of vendors we carry: ")
+
+        inputprompt = "Enter number of the Vendor you like best, if you dont know just choose the color you like best. Intel is blue, AMD is red: "
+        errormessage = "That input wasnt a CPU vendor we carry! \ntry again:"
+
+        for i in range(len(self.CPUVendors)):
+            print(f"[{i+1}]: {self.CPUVendors[i]}")
+
+        while True:
+            cpuBrand = int(input(inputprompt))
+
+            if cpuBrand == 1 or cpuBrand == 2:
+                return self.CPUVendors[cpuBrand-1]
+        
+
+
+#MotherboardType (Maby just let the choose if they want wifi or not at a $30 dollar discount if they dont to make it simpler)
+    def MotherboardTypeMenu(self) -> int:
+
+        print("Additionally You can pick whether or not you need your motherboard to support wifi. Here are your options to choose from: ")
+
+        inputprompt = "Enter name of the motherboard that tickles your fancy: "
+        errormessage = "That input wasnt a Motherboard we have in stock! \ntry again:"
+        
+        return Menu(self.Motherboards, inputprompt, errormessage)
+    
+#CPU(could do a specific SKU or just simplify and calculate cost baised on per core basis)
+    def cpuTypeMenu(self) -> int:
+        print("Here are the options for your CPU: ")
+
+        inputprompt = "Enter the CPU you want: "
+        errormessage = "That input is not a cpu we have in stock! \nTry again: "
+
+        return Menu(self.cpuOptions, inputprompt, errormessage)
+
+#CPU COOLER
+    def cpuCoolerMenu(self) -> int:
+        print("Pick which CPU cooler you want from the options below: ")
+
+        inputprompt = "Enter the CPU cooler you want: "
+        errormessage = "That input is not a cooler we have available"
+
+        return Menu(self.cpuCoolerOptions, inputprompt, errormessage)
+
+#GPU(Could do a specific SKU or just simplify and calculate cost baised on ram they want/need)
+    def gpuTypeMenu(self) -> int:
+        print("Here are the options for your GPU: ")
+
+        inputprompt = "Enter the name of the GPU you want: "
+        errormessage = "That input is not a GPU we have in stock! \nTry again: "
+
+        return Menu(self.gpuOptions, inputprompt, errormessage)
+
+#StorageType(the user can choose what type of storage they want)
+    def storageTypeMenu(self) -> int:
+        print("Here are the options for your storage: ")
+
+        inputprompt = "Enter the type of storage you want: "
+        errormessage = "That input is not a storagr type we have in stock! \nTry again: "
+
+        return Menu(self.storageoptions, inputprompt, errormessage)
+
+#memoryoption(the user can choose what type of memory they want)
+    def memoryoptionMenu(self) -> int:
+        print("Here are the options for your memory: ")
+
+        inputprompt = "Enter the type of memory you want: "
+        errormessage = "That input is not a memory type we have in stock! \nTry again: "
+
+        return Menu(self.memoryoptions, inputprompt, errormessage)
+
+
+
+#----------------------------
+#accessories start here
+
+    def RGBLightsMenu(self) -> int:
+        rgb = input("Do you want $40 worth of RGB leds installed inside your case? Y/N: ").lower()
+        if rgb == "y":
+            return 40
+        else:
+            return 0
+        
+    def preinstallWindows(self) -> int:
+        windows = input("Do you want your PC to come with Windows 11 Preinstalled? (-$100 if no) Y/N: ").lower()
+        if windows == "y":
+            return 0
+        else: 
+            return 100
+
+
+def Menu(dictionary:dict, inputprompt:str, errormessage:str):
+    """handles majority of menu making"""
+    while True:
+        print(FormatMenu(dictionary))
+
+        userinput = int(input(inputprompt))-1
+
+        """item = dictionary.get(userinput, errormessage)"""
+        item = list(dictionary.values())[userinput]
+        if item != errormessage:
+                return item
+        
+        print(errormessage)
+
+
+
+def FormatMenu(input:dict) -> str:
+    """
+        takes in dictionary
+        splits into index #, key and value
+        returns formatted menu
+    """
+    """makes a formatted menu when passed a dictionary"""
+    menustr = ""
+
+    for index, (key, value) in enumerate(input.items(), start=1):
+        menustr += f"[{index}]: {key}: ${value:.2f} \n"
+    return menustr
+        
+
+
+#
+#(HHD, SSD, NVME) (just do this calculation baised on cost per gigabyte)
---
+++ Week11/bobs_computers_app.py
@@ -0,0 +1,73 @@
+"""
+  Name: bobs_computers_app.py
+  Created: 10/11/23
+  Purpose: Main application file for menu and accessing objects
+"""
+
+# TODO: import class file
+import Bobs_computers_class
+
+# TODO: create main
+def main():
+    # Create computer object
+    computer = Bobs_computers_class.Computer()
+    # Call menu function, pass reference to computer object
+    menu(computer)
+
+# TODO: create menu function
+def menu(computer):
+    CPUbrand=""
+    while True:
+
+        computer.title()
+
+        runningtotal = 0
+
+        runningtotal += computer.ComputerCaseMenu()
+
+        CPUbrand = computer.CPUVendor()
+
+        if CPUbrand == "AMD":
+            CPUbrand = "AMD"
+        else:
+            CPUbrand = "Intel"
+
+        runningtotal += computer.MotherboardTypeMenu()
+
+        runningtotal += computer.cpuTypeMenu()
+
+        runningtotal += computer.cpuCoolerMenu()
+
+        runningtotal += computer.gpuTypeMenu()
+
+        runningtotal += computer.storageTypeMenu()
+
+        runningtotal += computer.memoryoptionMenu()
+
+        accessories = input("\nThat concludes all the nessesary things when building a tower, do you want to add extra accessories (y/n): ").lower()
+
+        if accessories == "y":
+            runningtotal += computer.RGBLightsMenu()
+            runningtotal -= computer.preinstallWindows()
+
+        print(f"\nYour final bill for your {CPUbrand} computer is ${runningtotal}.")
+
+        computer.quantity = (int(input("How many copies of this computer are you going to buy?: ")))
+        # Get number of computers from user
+        # set quantity to object
+
+        # Get computer quantity from object
+        quantity = computer.quantity
+        print(f" You ordered {quantity} computer(s), costing ${runningtotal} each")
+        print(f" TOTAL COST: ${quantity * runningtotal}")
+
+        # input to ask if user wants program to keep running
+        run_again = input("Do you wish to run again? [Y/N]: ").lower()
+        if run_again != "y":
+            break
+
+
+
+
+if __name__ == "__main__":
+    main()
