60357ae359d964cdd7674a50229e05dfbaa20566
siah.drews <siah.drews@gmail.com>
2023-11-18 12:26:53 -0700

    shipping disaster

--- Week13/bobs_disasters_class.py
+++ Week13/bobs_disasters_class.py
@@ -29,3 +29,13 @@ class Disasters:
             
         return colorchoosen, colorchoosen[1]
     
+    def failedDelivery(self, premium_shipping, warranty):
+        if warranty == "n":
+            if premium_shipping == "n":
+                river = random.randint(1,10)
+                if river <= 3:
+                    return("Uh Oh!, the UPS Driver drove into a river and destroyed your PC")
+            elif premium_shipping == "y":
+                river = random.randint(1,10)
+                if river == 1:
+                    return("Uh Oh!, the UPS Driver drove into a river and destroyed your PC")
