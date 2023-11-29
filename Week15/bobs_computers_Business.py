fe66f1377c5e89290d73079f331b04271ae30601
Josiah Andrews <siah.drews@gmail.com>
2023-11-29 14:37:46 -0700

    commented business and disasters

--- Week15/bobs_computers_Business.py
+++ Week15/bobs_computers_Business.py
@@ -1,34 +1,38 @@
 
 """
     Name: bobs_computer_Business.py
-    Created: 11/1/23
+    Created: 11/29/23
     Purpose: class file for tracking profit/loss/sales
 """
 class Business:
-    def __init__(self, sales=0, costs=0, profit=0):
+    def __init__(self, sales=0, costs=0, profit=0): # set default values to 0
         self.totalSales = sales
         self.totalCosts = costs
         self.totalProfit = profit
 
     #------------SALES--------------
+        # sales getter
     def returnSales(self):
         return self.totalSales
     
+        # sales setter
     def trackSales(self, runningTotal):
-        self.totalSales += runningTotal     
+        self.totalSales += runningTotal     # add running total to total sales 
     
     #---------------COSTS----------------
+        # costs getter
     def returnCosts(self):
         return self.totalCosts
     
+        #costs setter
     def trackCosts(self):
-        self.totalCosts = (self.totalSales / 5)
+        self.totalCosts = (self.totalSales / 5) #costs are total sales / 5
     
     #--------------PROFIT--------------#
+        # profit getter
     def returnProfit(self):
         return(self.totalProfit)
-    
-    def trackProfit(self):
-        self.totalProfit = self.totalSales - self.totalCosts
 
-    
+        # profit setter
+    def trackProfit(self):
+        self.totalProfit = self.totalSales - self.totalCosts # profit is total sales
--- Week15/bobs_disasters_class.py
+++ Week15/bobs_disasters_class.py
@@ -1,49 +1,52 @@
 import random
 
 class Disasters:
-    def __init__(self) -> None:
+    def __init__(self) -> None: # empty __init__ function
         pass
 
     def ShipIncorrectColor(self, colors, colorchoosen):
-        disasterchance = random.randint(1, 2)
+        disasterchance = random.randint(1, 2) # generate random chance for disaster
 
         if disasterchance == 1:
-            randomcolor = random.randint(0,len(colors)-1)
+            randomcolor = random.randint(0,len(colors)-1) #if disaster chance is 1: set random case color to random color
 
 
-            keysbad = colors.keys()
-            keygood = []
+            keysbad = colors.keys() # assign keysbad to the keys of the colors
+            keygood = [] # empty keygood list
 
-            for key in keysbad:
+            for key in keysbad: # loop to add the keys to the keygood list
                     keygood.append(key)
 
-            newcasecolor = keygood[randomcolor]
-            newcaseprice = colors.get(newcasecolor)
+            newcasecolor = keygood[randomcolor] #set new case color to randomly chosen new color
+            newcaseprice = colors.get(newcasecolor) # set case price to price for the new case color
 
 
-            if colorchoosen[0] != newcasecolor:
+            if colorchoosen[0] != newcasecolor: # if the randomly chosen case color is not the new case color
+                # display that the wrong color was chosen
                 print("\n#--------------------------------------------------------------------------------------------#")
                 print(f"DISASTER HAS STRUCK:\n You chose {colorchoosen[0]} but somehow you ended up with {newcasecolor}")
                 print("#----------------------------------------------------------------------------------------------#\n")
-                return [newcasecolor, newcaseprice], newcaseprice
+                return [newcasecolor, newcaseprice], newcaseprice # return new case color/price
             
-        return colorchoosen, colorchoosen[1]
+        return colorchoosen, colorchoosen[1] # return  original color if random disaster chance doesn't happen
     
-    def failedDelivery(self, premium_shipping, warranty):
+    # determine if the item is properly shipped to the user, or if the delivery failes (UPS driver drives into river)
+    # rates are extremely bad, because it is funny
+    def failedDelivery(self, premium_shipping, warranty): 
         if warranty == "n":
-            if premium_shipping == "n":
+            if premium_shipping == "n": # no warranty or premium shipping
                 river = random.randint(1,10)
-                if river <= 3:
+                if river <= 3: # 30% chance for failed delivery
                     return("Uh Oh!, the UPS Driver drove into a river and destroyed your PC")
                 else:
                     return("Your package was properly shipped")
-            elif premium_shipping == "y":
+            elif premium_shipping == "y": # no warranty, but has premium shipping
                 river = random.randint(1,10)
-                if river == 1:
+                if river == 1: # 10% chance for failed delivery
                     return("Uh Oh!, the UPS Driver drove into a river and destroyed your PC")
                 else:
                     return("Your package was properly shipped")
-        else:
+        else: # if they have warranty, the package ships properly
             return("Your package was properly shipped")
         
     def WrongStorageOption(self, premium_shipping, warranty):
